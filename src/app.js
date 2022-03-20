6eaa0e6313e881cd52b1c87aa15bb77557921b04
adameichelkraut <adam.eichelkraut@compozelabs.com>
2022-03-20 13:59:05 -0500

    feedback post gives 201 unless theres a validation problem

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
-    res.json("Hello World!")
+app.post("/feedback", (req, res) => {
+    const body = req.body
+
+    if (body.name === "" || body.name === undefined
+    || body.comments === "" || body.comments === undefined) {
+        res.statusCode = 400
+    } else {
+        res.statusCode = 201
+    }
+
+    res.send()
 })
 
 module.exports = app
--- src/app.test.js
+++ src/app.test.js
@@ -4,9 +4,61 @@ const supertest = require("supertest")
 describe("the backend", () => {
     const requestWithSupertest = supertest(app)
     
-    it("says hello world!", async () => {
-        const res = await requestWithSupertest.get("/")
-        expect(res.status).toBe(200)
-        expect(res.body).toBe("Hello World!")
+    it("accepts a feedback request", async () => {
+        const res = await requestWithSupertest
+            .post("/feedback")
+            .set("Content-Type", "application/json")
+            .send(JSON.stringify({
+                name: "Bilbo Baggins",
+                comments: "Was a hobbit from the Shire."
+            }))
+
+        expect(res.status).toBe(201)
+    })
+
+    it("rejects a feedback request if name is blank or missing", async () => {
+        const res = await requestWithSupertest
+            .post("/feedback")
+            .set("Content-Type", "application/json")
+            .send(JSON.stringify({
+                name: "",
+                comments: "Was a hobbit from the Shire."
+            }))
+
+        expect(res.status).toBe(400)
+    })
+
+    it("rejects a feedback request if name is missing", async () => {
+        const res = await requestWithSupertest
+            .post("/feedback")
+            .set("Content-Type", "application/json")
+            .send(JSON.stringify({
+                comments: "Was a hobbit from the Shire."
+            }))
+
+        expect(res.status).toBe(400)
+    })
+
+    it("rejects a feedback request if comment is blank", async () => {
+        const res = await requestWithSupertest
+            .post("/feedback")
+            .set("Content-Type", "application/json")
+            .send(JSON.stringify({
+                name: "Bilbo Baggins",
+                comments: ""
+            }))
+
+        expect(res.status).toBe(400)
+    })
+
+    it("rejects a feedback request if comment is missing", async () => {
+        const res = await requestWithSupertest
+            .post("/feedback")
+            .set("Content-Type", "application/json")
+            .send(JSON.stringify({
+                name: "Bilbo Baggins"
+            }))
+
+        expect(res.status).toBe(400)
     })
 })
